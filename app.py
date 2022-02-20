af82d83d8fad0859f2c84011747d2ca39f44171c
Abhijeet <abhigupta7b@gmail.com>
2022-02-20 16:46:20 +0530

    heroku

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template, Response, url_for
+from flask import Flask, render_template, Response, url_for, redirect
 import cv2 as cv
 from face_detector import face_detector
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'password'
 
 
-@app.route('/')
+class InfoForm(FlaskForm):
+    submit = SubmitField(label="Submit")
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = InfoForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+
+    return render_template('home.html', form=form)
+
+
+@app.route('/home',methods=['GET', 'POST'])
+def home():
     return render_template('index.html')
 
-
-@app.route('/video')
+@app.route('/video',methods=['GET', 'POST'])
 def video():
     vid = cv.VideoCapture(0)
     success, dfs = vid.read()
