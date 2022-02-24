f46be924bfee5ea6e219521eb2953fb805065abe
Abhijeet <abhigupta7b@gmail.com>
2022-02-24 17:00:22 +0530

    Fixed webcam Bug

--- app.py
+++ app.py
@@ -1,37 +1,60 @@
-from flask import Flask, render_template, Response, url_for, redirect
-import cv2 as cv
+import base64
+import cv2
+import io
+
+import numpy as np
+from PIL import Image
+from engineio.payload import Payload
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 from face_detector import face_detector
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
+
+Payload.max_decode_packets = 2048
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'password'
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 
-class InfoForm(FlaskForm):
-    submit = SubmitField(label="Submit")
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    form = InfoForm()
-
-    if form.validate_on_submit():
-        return redirect(url_for('home'))
-
-    return render_template('home.html', form=form)
-
-
-@app.route('/home',methods=['GET', 'POST'])
-def home():
     return render_template('index.html')
 
-@app.route('/video',methods=['GET', 'POST'])
-def video():
-    vid = cv.VideoCapture(0)
-    success, dfs = vid.read()
-    if success:
-        return Response(face_detector(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def readb64(base64_string):
+    idx = base64_string.find('base64,')
+    base64_string = base64_string[idx + 7:]
+
+    sbuf = io.BytesIO()
+
+    sbuf.write(base64.b64decode(base64_string, ' /'))
+    pimg = Image.open(sbuf)
+
+    return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@socketio.on('catch-frame')
+def catch_frame(data):
+    emit('response_back', data)
+
+
+
+@socketio.on('image')
+def image(data_image):
+    frame = (readb64(data_image))
+    frame = face_detector(frame)
+
+    imgencode = cv2.imencode('.jpeg', frame, [cv2.IMWRITE_JPEG_QUALITY, 40])[1]
+
+    # base64 encode
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpeg;base64,'
+    stringData = b64_src + stringData
+
+    # emit the frame back
+    emit('response_back', stringData)
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, port=9990, debug=True)
--- face_detector.py
+++ face_detector.py
@@ -1,43 +1,36 @@
 import cv2 as cv
-import time
 import mediapipe as mp
 
-def face_detector() :
-    pTime = 0
+
+def face_detector(img):
+
     video = cv.VideoCapture(0)
     mpFaceDetection = mp.solutions.face_detection
-    mpDraw = mp.solutions.drawing_utils
     faceDetection = mpFaceDetection.FaceDetection()
 
-    while True:
-        success, img = video.read()
-        if not success :
-            return "Please turn on your camera and refresh the page"
-        else:
-            cTime = time.time()
-            fps = 1 / (cTime - pTime)
-            img = cv.flip(img, 1)
-            results = faceDetection.process(img)
-            if results.detections:
-                for id, detection in enumerate(results.detections):
-                    if detection.score[0] > 0.50:
-                        bboxc = detection.location_data.relative_bounding_box
-                        ih, iw, ic = img.shape
-                        bbox = int(bboxc.xmin * iw), int(bboxc.ymin * ih), int(bboxc.width * iw), int(bboxc.height * ih)
-                        cv.rectangle(img, bbox, (0, 255, 0), 2)
-                        cv.putText(img, f"{int(detection.score[0] * 100)}%",
-                                   (int(bboxc.xmin * iw), int(bboxc.ymin * ih-15)),
-                                   cv.FONT_ITALIC, 1.5, (255, 255, 255), 2)
-
-            cv.putText(img, f"FPS:{int(fps)}", (20, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
-            pTime = cTime
-            if cv.waitKey(30) and 0xFF == ord('d'):
-                break
-
-            ret, buffer = cv.imencode('.jpg', img)
-            frame = buffer.tobytes()
-
-            yield b'--frame\r\n'\
-                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+    img = cv.flip(img, 1)
+    results = faceDetection.process(img)
+    if results.detections:
+        for id, detection in enumerate(results.detections):
+            if detection.score[0] > 0.50:
+                bboxc = detection.location_data.relative_bounding_box
+                ih, iw, ic = img.shape
+                bbox = int(bboxc.xmin * iw), int(bboxc.ymin * ih), int(bboxc.width * iw), int(bboxc.height * ih)
+                cv.rectangle(img, bbox, (0, 255, 0), 2)
+                cv.putText(img, f"{int(detection.score[0] * 100)}%",
+                           (int(bboxc.xmin * iw), int(bboxc.ymin * ih - 15)),
+                           cv.FONT_ITALIC, 1.5, (255, 255, 255), 2)
 
     cv.waitKey(0)
+    return img
+
+def readb64(base64_string):
+    idx = base64_string.find('base64,')
+    base64_string = base64_string[idx + 7:]
+
+    sbuf = io.BytesIO()
+
+    sbuf.write(base64.b64decode(base64_string, ' /'))
+    pimg = Image.open(sbuf)
+
+    return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
