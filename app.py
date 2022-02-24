451b811304238f7e4ae9c0fc330ef4f31ee260ed
Abhijeet <abhigupta7b@gmail.com>
2022-02-24 17:11:21 +0530

    fixed webcam results canvas

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 from engineio.payload import Payload
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-from face_detector import face_detector
+import mediapipe as mp
 
 Payload.max_decode_packets = 2048
 
@@ -54,6 +54,26 @@ def image(data_image):
     # emit the frame back
     emit('response_back', stringData)
 
+
+def face_detector(img):
+    mpFaceDetection = mp.solutions.face_detection
+    faceDetection = mpFaceDetection.FaceDetection()
+
+    img = cv2.flip(img, 1)
+    results = faceDetection.process(img)
+    if results.detections:
+        for id, detection in enumerate(results.detections):
+            if detection.score[0] > 0.50:
+                bboxc = detection.location_data.relative_bounding_box
+                ih, iw, ic = img.shape
+                bbox = int(bboxc.xmin * iw), int(bboxc.ymin * ih), int(bboxc.width * iw), int(bboxc.height * ih)
+                cv2.rectangle(img, bbox, (0, 255, 0), 2)
+                cv2.putText(img, f"{int(detection.score[0] * 100)}%",
+                            (int(bboxc.xmin * iw), int(bboxc.ymin * ih - 15)),
+                            cv2.FONT_ITALIC, 1.5, (255, 255, 255), 2)
+
+    cv2.waitKey(0)
+    return img
 
 
 if __name__ == '__main__':
